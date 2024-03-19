2d3a45b84ac41d5450c1d7ed606565d490ac13e6
smebes <murat.ebes@theautobids.com>
2024-03-19 12:36:33 +0300

    Added database connections

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
 
-// 0062060619
-// 1250773628
-// 1250855578
-// 0316422967
-// 0316444006
+let isStopped = false;
+
+function stopProcessing() {
+    isStopped = true;
+    if (currentProcessingTabId !== null) {
+        chrome.tabs.remove(currentProcessingTabId); 
+        currentProcessingTabId = null;
+    }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "stop") {
+        stopProcessing();
+    }
+});
 
 
 chrome.action.onClicked.addListener((tab) => {
@@ -18,80 +28,146 @@ function setPageBackgroundColor() {
 }
 
 
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+    if (tabId === currentProcessingTabId) {
+        console.log('İşlem gören sekme kapatıldı, işlem durduruluyor.');
+        stopProcessing();
+    }
+});
+
 let processingQueue = [];
 let isProcessing = false;
 
 function processNextItem() {
-  if (processingQueue.length === 0) {
-      isProcessing = false;
-      return;
-  }
-  
-  isProcessing = true;
-  const asin = processingQueue.shift(); 
+    if (isStopped || processingQueue.length === 0) {
+        isProcessing = false;
+        processingQueue = []; 
+        currentProcessingTabId = null;
+        return;
+    }
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      const currentTab = tabs[0]; 
+    isProcessing = true;
+    const asin = processingQueue.shift(); 
 
-      chrome.tabs.update(currentTab.id, {url: `https://www.amazon.com/dp/${asin}`}, function(currentTab) {
-          chrome.tabs.onUpdated.addListener(function updated(tabId, changeInfo, currentTab) {
-              if (tabId === currentTab.id && changeInfo.status === 'complete') {
-                  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                  
-                      chrome.scripting.executeScript({
-                          target: {tabId: currentTab.id},
-                          function: getProductDetailsForDownload
-                      }, (injectionResults) => {
-                            if (injectionResults && injectionResults[0] && injectionResults[0].result) {
-                              const details = injectionResults[0].result;
-                              const blob = new Blob([details], {type: 'text/plain;charset=utf-8'});
-                              const reader = new FileReader();
-                              reader.onload = function() {
-                                  chrome.downloads.download({
-                                      url: reader.result,
-                                      filename: asin ? `${asin}.txt` : 'details.txt'
-                                  });
-                              };
-                              reader.readAsDataURL(blob);
-                          }
-                      });
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        const currentTab = tabs[0]; 
+
+        chrome.tabs.update(currentTab.id, {url: `https://www.amazon.com.tr/dp/${asin}`}, function(currentTab) {
+            chrome.tabs.onUpdated.addListener(function updated(tabId, changeInfo, currentTab) {
+                if (tabId === currentTab.id && changeInfo.status === 'complete') {
+                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                    chrome.scripting.executeScript({
+                        target: {tabId: currentTab.id},
+                        function: postDatabase
+                    }, (injectionResults) => {
+                        if (injectionResults && injectionResults[0] && injectionResults[0].result) {
+                            const productData = injectionResults[0].result;
+
+                            fetch('http://127.0.0.1:5000/add-product', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(productData),
+                            })
+                            .then(response => response.json())
+                            .then(data => console.log('Ürün başarıyla eklendi:', data))
+                            .catch((error) => console.error('Hata:', error));
+
+                            
+                        }
+                    });
+                    chrome.scripting.executeScript({
+                            target: {tabId: currentTab.id},
+                            function: getProductDetailsForDownload
+                        }, (injectionResults) => {
+                            if (injectionResults && injectionResults[0] && injectionResults[0].result && injectionResults[0].result !== "Title not found") {
+                            const details = injectionResults[0].result;
+                            const asin = getASINFromUrl(currentTab.url);
+                            const blob = new Blob([details], {type: 'text/plain;charset=utf-8'});
+                            const reader = new FileReader();
+                            reader.onload = function() {
+                                chrome.downloads.download({
+                                    url: reader.result,
+                                    filename: asin ? `${asin}.txt` : 'details.txt'
+                                });
+                            };
+                            reader.readAsDataURL(blob);
+                            } else {
+                            console.log('Ürün bulunamadı, indirme yapılmıyor.');
+                            }
+                        });
                     });
                     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                      const tab = tabs[0];
-                      chrome.scripting.executeScript({
-                          target: {tabId: tab.id},
-                          function: getImageUrlForDownload
-                      }, (injectionResults) => {
-                          const asin = getASINFromUrl(tab.url);
-                          for (const frameResult of injectionResults)
-                              if (frameResult.result) {
-                                  chrome.downloads.download({
-                                      url: frameResult.result,
-                                      filename: asin ? `${asin}.jpg` : 'downloaded-image.jpg'
-                                  });
-                              }
-                      });
+                        const tab = tabs[0];
+                        chrome.scripting.executeScript({
+                            target: {tabId: tab.id},
+                            function: getImageUrlForDownload
+                        }, (injectionResults) => {
+                            const asin = getASINFromUrl(tab.url);
+                            for (const frameResult of injectionResults)
+                                if (frameResult.result) {
+                                    chrome.downloads.download({
+                                        url: frameResult.result,
+                                        filename: asin ? `${asin}.jpg` : 'downloaded-image.jpg'
+                                    });
+                                }
+                        });
                     });  
+                    
 
-                  chrome.tabs.onUpdated.removeListener(updated);
-                  if (processingQueue.length > 0) {
-                      processNextItem();
-                  } else {
-                      isProcessing = false;
-                  }
-              }
-          });
-      });
-  });
-  
+                    chrome.tabs.onUpdated.removeListener(updated);
+                    if (processingQueue.length > 0 && !isStopped) {
+                        processNextItem();
+                    } else {
+                        isProcessing = false;
+                        chrome.tabs.remove(currentProcessingTabId); // İşlem durdurulduğunda sekme kapatılır
+                        currentProcessingTabId = null;
+                    }
+                }
+            });
+        });
+    });
+}
+
+function postDatabase() {
+    let details = '';
+    document.querySelectorAll('#detailBullets_feature_div .a-list-item, #productOverview_feature_div .a-list-item').forEach(item => {
+        const textContent = item.textContent.trim();
+        if (textContent) {
+            details += `${textContent.replace(/\s+/g, ' ')}\n`; 
+        }
+    });
+
+    return {
+        name: document.getElementById('productTitle') ? document.getElementById('productTitle').textContent.trim() : "Title not found",
+        description: details,
+        price: "99.99"
+    };
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "fetchDetails") {
-        processingQueue.push(...request.asinList); 
+        isStopped = false; 
+        processingQueue = [...request.asinList];
         if (!isProcessing) {
-            processNextItem(); 
+            processNextItem();
         }
+    }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "sendProductData1") {
+        fetch('http://127.0.0.1:5000/add-product', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(request.data),
+        })
+        .then(response => response.json())
+        .then(data => console.log('Ürün başarıyla eklendi:', data))
+        .catch((error) => console.error('Hata:', error));
     }
 });
 
@@ -108,26 +184,32 @@ function getImageUrlForDownload() {
 
 
 function getProductDetailsForDownload() {
-  let details = `Title: ${document.getElementById('productTitle') ? document.getElementById('productTitle').textContent.trim() : "Title not found"}\n`;
+    if (document.getElementById('productTitle')) {
+        let details = `Title: ${document.getElementById('productTitle') ? document.getElementById('productTitle').textContent.trim() : "Title not found"}\n`;
 
-  document.querySelectorAll('#detailBullets_feature_div .a-list-item, #productOverview_feature_div .a-list-item').forEach(item => {
-      const textContent = item.textContent.trim();
-      if (textContent) {
-          details += `${textContent.replace(/\s+/g, ' ')}\n`; // Fazladan boşlukları temizle
-      }
-  });
+        document.querySelectorAll('#detailBullets_feature_div .a-list-item, #productOverview_feature_div .a-list-item').forEach(item => {
+            const textContent = item.textContent.trim();
+            if (textContent) {
+                details += `${textContent.replace(/\s+/g, ' ')}\n`; // Fazladan boşlukları temizle
+            }
+        });
 
-  const bestSellersRank = document.querySelector("#SalesRank") ? document.querySelector("#SalesRank").textContent.trim() : "";
-  if (bestSellersRank) {
-      details += `Best Sellers Rank: ${bestSellersRank.replace(/\s+/g, ' ')}\n`;
-  }
+        const bestSellersRank = document.querySelector("#SalesRank") ? document.querySelector("#SalesRank").textContent.trim() : "";
+        if (bestSellersRank) {
+            details += `Best Sellers Rank: ${bestSellersRank.replace(/\s+/g, ' ')}\n`;
+        }
 
-  const customerReviews = document.querySelector("#acrCustomerReviewText") ? document.querySelector("#acrCustomerReviewText").textContent.trim() : "";
-  if (customerReviews) {
-      details += `Customer Reviews: ${customerReviews.replace(/\s+/g, ' ')}\n`;
-  }
+        const customerReviews = document.querySelector("#acrCustomerReviewText") ? document.querySelector("#acrCustomerReviewText").textContent.trim() : "";
+        if (customerReviews) {
+            details += `Customer Reviews: ${customerReviews.replace(/\s+/g, ' ')}\n`;
+        }
 
-  return details;
+        return details;
+       
+    } else {
+        return "Title not found";
+    }
+  
 }
 
 
--- popup.js
+++ popup.js
@@ -15,32 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
-    document.getElementById('downloadButton').addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            const tab = tabs[0]; // Aktif sekmeyi al
-            const asin = getASINFromUrl(tab.url); // ASIN numarasını al
     
-            chrome.scripting.executeScript({
-                target: {tabId: tab.id},
-                function: getProductDetailsForDownload
-            }, (injectionResults) => {
-                for (const frameResult of injectionResults)
-                    if (frameResult.result) {
-                        const details = frameResult.result;
-                        const blob = new Blob([details], {type: 'text/plain;charset=utf-8'});
-                        const url = URL.createObjectURL(blob);
-                        const a = document.createElement('a');
-                        a.href = url;
-                        // ASIN varsa dosya adını ASIN ile belirle, yoksa varsayılan ismi kullan
-                        a.download = asin ? `${asin}.txt` : '1.txt';
-                        document.body.appendChild(a);
-                        a.click();
-                        document.body.removeChild(a);
-                        URL.revokeObjectURL(url);
-                    }
-            });
-        });
-    });
     document.getElementById('downloadImageButton').addEventListener('click', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             const tab = tabs[0];
@@ -63,9 +38,67 @@ document.addEventListener('DOMContentLoaded', function() {
         const asinList = document.getElementById('asinList').value.split('\n').filter(Boolean);
         chrome.runtime.sendMessage({action: "fetchDetails", asinList: asinList});
     });
+    document.getElementById('stopButton').addEventListener('click', function() {
+        chrome.runtime.sendMessage({action: "stop"});
+    });
+    document.getElementById('downloadButton').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            const tab = tabs[0]; 
+            const asin = getASINFromUrl(tab.url); 
     
-      
+            chrome.scripting.executeScript({
+                target: {tabId: tab.id},
+                function: getProductDetailsForDownload
+            }, (injectionResults) => {
+                for (const frameResult of injectionResults)
+                    if (frameResult.result) {
+                        const details = frameResult.result;
+                        const blob = new Blob([details], {type: 'text/plain;charset=utf-8'});
+                        const url = URL.createObjectURL(blob);
+                        const a = document.createElement('a');
+                        a.href = url;
+                        a.download = asin ? `${asin}.txt` : '1.txt';
+                        document.body.appendChild(a);
+                        a.click();
+                        document.body.removeChild(a);
+                        URL.revokeObjectURL(url);
+                    }
+            });
+        });
+    });
+    document.getElementById('databaseButton').addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                function: postDatabase
+            }, (injectionResults) => {
+                if (injectionResults && injectionResults[0] && injectionResults[0].result) {
+                    chrome.runtime.sendMessage({
+                        action: "sendProductData",
+                        data: injectionResults[0].result
+                    });
+                }
+            });
+        });
+    });
 });
+
+function postDatabase() {
+    details = ''
+    document.querySelectorAll('#detailBullets_feature_div .a-list-item, #productOverview_feature_div .a-list-item').forEach(item => {
+        const textContent = item.textContent.trim();
+        if (textContent) {
+            details += `${textContent.replace(/\s+/g, ' ')}\n`; 
+        }
+    });
+    return {
+        name: `${document.getElementById('productTitle') ? document.getElementById('productTitle').textContent.trim() : "Title not found"}`,
+        description: details,
+        price: "100"
+    };
+}
+
+
 
 
 function getImageUrlForDownload() {
@@ -82,15 +115,13 @@ function getImageUrlForDownload() {
 function getProductDetailsForDownload() {
     let details = `Title: ${document.getElementById('productTitle') ? document.getElementById('productTitle').textContent.trim() : "Title not found"}\n`;
 
-    // Ürün detaylarını al
     document.querySelectorAll('#detailBullets_feature_div .a-list-item, #productOverview_feature_div .a-list-item').forEach(item => {
         const textContent = item.textContent.trim();
         if (textContent) {
-            details += `${textContent.replace(/\s+/g, ' ')}\n`; // Fazladan boşlukları temizle
+            details += `${textContent.replace(/\s+/g, ' ')}\n`; 
         }
     });
 
-    // Diğer spesifik bilgileri al
     const bestSellersRank = document.querySelector("#SalesRank") ? document.querySelector("#SalesRank").textContent.trim() : "";
     if (bestSellersRank) {
         details += `Best Sellers Rank: ${bestSellersRank.replace(/\s+/g, ' ')}\n`;
